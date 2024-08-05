596e162cf714ca8a0567791648cf7a726eed662b
laijinlin <laijinlin2020@gmail.com>
2024-08-05 11:06:03 -0400

    passed tests

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,42 +1,37 @@
-import torch
+import jax.numpy as jnp
+from jax import random
 import pytest
-from heterogeneity_gym import rectangle
+from src.heterogeneity_gym import rectangle
 
-if torch.cuda.is_available():
-    devices = ["cuda", "cpu"]
-else:
-    devices = ["cpu",]
-
-
-@pytest.mark.parametrize("device", devices)
-def test_normalization(device):
-    latent_values = torch.randn(8, 2, device=device)
+def test_normalization():
+    key = random.PRNGKey(0)
+    latent_values = random.normal(key, (8, 2))
     rectangle_model = rectangle.RectangleModel(None)
     images = rectangle_model.render_images_from_latent(latent_values)[0]
 
     dx = rectangle_model.grid_ticks[1] - rectangle_model.grid_ticks[0]
 
-    image_integral = torch.sum(images, dim=(1, 2)) * dx**2
-    avg_integral = torch.mean(image_integral)
-    assert torch.all((image_integral - avg_integral) < 2e-3)
+    image_integral = jnp.sum(images, axis=(1, 2)) * dx**2
+    avg_integral = jnp.mean(image_integral)
+    assert jnp.all((image_integral - avg_integral) < 2e-3)
 
 
-@pytest.mark.parametrize("device", devices)
-def test_square_images_are_symmetric(device):
-    latent_values = torch.randn(16, device=device)
-    latent_values = torch.stack([latent_values, latent_values], dim=1)
+def test_square_images_are_symmetric():
+    key = random.PRNGKey(0)
+    latent_values = random.normal(key, (16, ))
+    latent_values = jnp.stack([latent_values, latent_values], axis=1)
     rectangle_model = rectangle.RectangleModel(None)
     images = rectangle_model.render_images_from_latent(latent_values)[0]
     print(images.shape)
-    image_transpose = torch.transpose(images, 1, 2)
-    assert torch.allclose(images, image_transpose)
+    image_transpose = jnp.transpose(images, (0, 2, 1))
+    assert jnp.allclose(images, image_transpose)
 
 
-@pytest.mark.parametrize("device", devices)
-def test_displacement_from_center_constant_magnitude(device):
-    latent_values = torch.randn(8, 2, device=device)
+def test_displacement_from_center_constant_magnitude():
+    key = random.PRNGKey(0)
+    latent_values = random.normal(key, (8, 2))
     rectangle_model = rectangle.RectangleModel(None)
     structures = rectangle_model.construct_structures(latent_values)
-    abs_displacements = torch.abs(structures)
-    diff_in_displacements = abs_displacements - abs_displacements[:, 0, :].unsqueeze(1)
-    assert torch.all(diff_in_displacements < 1e-6)
+    abs_displacements = jnp.abs(structures)
+    diff_in_displacements = abs_displacements - jnp.expand_dims(abs_displacements[:, 0, :], 1)
+    assert jnp.all(diff_in_displacements < 1e-6)
